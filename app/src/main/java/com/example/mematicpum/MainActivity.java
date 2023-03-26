4f917d19786fb1e9978e03f67547f3d01df2cce4
Bartlomiej-Kozlowski <104801@stud.uz.zgora.pl>
2023-03-27 00:09:13 +0200

    Create repository Bind firebase service

---
+++ app/src/main/java/com/example/mematicpum/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mematicpum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
